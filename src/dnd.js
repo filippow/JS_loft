8bd6f366b27b8476258b7130a062c6f9a4cfc699
filippow <filippow.ewgen@yandex.ru>
2018-08-09 00:23:26 +0300

    TASK_5 DONE

---
+++ src/dnd.js
@@ -0,0 +1,100 @@
+/* Задание со звездочкой */
+
+/*
+ Создайте страницу с кнопкой.
+ При нажатии на кнопку должен создаваться div со случайными размерами, цветом и позицией на экране
+ Необходимо предоставить возможность перетаскивать созданные div при помощи drag and drop
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+const homeworkContainer = document.querySelector('#homework-container');
+
+/*
+ Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
+ Функция должна только создавать элемент и задвать ему случайные размер/позицию/цвет
+ Функция НЕ должна добавлять элемент на страницу. На страницу элемент добавляется отдельно
+
+ Пример:
+   const newDiv = createDiv();
+   homeworkContainer.appendChild(newDiv);
+ */
+function createDiv() {
+    let div = document.createElement('div'),
+        rand = function(n) {
+            return Math.floor(Math.random() * n);
+        },
+        config = {
+            width: rand(500) + 'px',
+            height: rand(500) + 'px',
+            top: rand(500) + 'px',
+            left: rand(500) +'px',
+            backgroundColor: `rgb(${rand(256)}, ${rand(256)}, ${rand(256)})`,
+            position: 'absolute',
+            cursor: 'pointer'
+        };
+
+    div.classList.add('draggable-div');
+    for (let key in config) {
+        if (config.hasOwnProperty(key)) {
+            div.style[key] = config[key];
+        }
+    }
+
+    return div;
+}
+
+/*
+ Функция должна добавлять обработчики событий для перетаскивания элемента при помощи drag and drop
+
+ Пример:
+   const newDiv = createDiv();
+   homeworkContainer.appendChild(newDiv);
+   addListeners(newDiv);
+ */
+function addListeners(target) {
+    target.onmousedown = function(e) {
+    
+        var shiftX = e.pageX-target.getBoundingClientRect().left + pageXOffset;
+        var shiftY = e.pageY-target.getBoundingClientRect().top + pageYOffset;
+          
+        document.onmousemove = function(e) {
+            moveRectangle(e)
+        }
+
+        document.onmouseup = function() {
+            document.onmousemove = null;
+        }
+      
+        function moveRectangle(event) {
+            target.style.top = event.pageY-shiftY + 'px';
+            target.style.left = event.pageX - shiftX + 'px';
+        }
+    }
+}
+
+let addDivButton = homeworkContainer.querySelector('#addDiv');
+
+addDivButton.addEventListener('click', function() {
+    // создать новый div
+    const div = createDiv();
+
+    // добавить на страницу
+    homeworkContainer.appendChild(div);
+    // назначить обработчики событий мыши для реализации D&D
+    
+    addListeners(div);
+    // можно не назначать обработчики событий каждому div в отдельности, а использовать делегирование
+    // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
+});
+
+export {
+    createDiv
+};
